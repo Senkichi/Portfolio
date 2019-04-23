489d2c34f8cce731531fd2e25337aa72c935fb29
S Martin <samuel.senkichi.martin@gmail.com>
2019-04-23 16:33:12 -0700

    initial portfolio commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, jsonify
+from flask_pymongo import PyMongo
+from urllib.parse import quote_plus
+import pymongo
+import json
+from bson.json_util import dumps
+from flask_cors import CORS
+
+app = Flask(__name__,template_folder='static/templates')
+
+
+#allow cross origin script access so can access via js
+CORS(app)
+
+mongo = PyMongo(app, uri="mongodb://root:ucbmongodb@35.184.4.63:27017/weather")
+conn = 'mongodb://root:ucbmongodb@35.184.4.63:27017'
+client = pymongo.MongoClient(conn)
+db = client.weather
+
+
+# Set up routes
+@app.route("/")
+@app.route("/index")
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/tcc")
+def tcc():
+    return render_template("tcc.html")
+
+@app.route("/tcc-documentation")
+def tcc_doc():
+    return render_template("tcc_documentation.html")
+
+# Set up api routes
+@app.route("/api/v1/<year>/<severity>/<algorithm>")
+def firePrediction(year,severity,algorithm):
+    print("reached the API")
+    collection_name = severity + '_' +algorithm + '_' + year
+    predictionData = dumps(db[collection_name].find({},{'_id': 0,'x':1,'y':1,'prediction':1}))
+    print(predictionData)
+    return jsonify(json.loads(predictionData))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
